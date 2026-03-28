fb1747c3b0c1e8f4d2c9ecf0e6ff7b38cdf3f9b2
Mr Pepsi <MrPepsi1069@gmail.com>
2026-03-27 17:31:03 -0700

    Add /templateuse command

--- bot.py
+++ bot.py
@@ -110,7 +110,7 @@ COGS = [
     "cogs.dmmembers", "cogs.premium", "cogs.awardcheck", "cogs.award",
     "cogs.ringadd", "cogs.lineup", "cogs.depthchart", "cogs.league",
     "cogs.gametime", "cogs.times", "cogs.contract",
-    "cogs.globalannouncement",
+    "cogs.globalannouncement", "cogs.templateuse",
     "cogs.logging",
 ]
 
